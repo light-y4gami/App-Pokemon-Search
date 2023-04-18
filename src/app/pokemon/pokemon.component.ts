a4d6278498414d2eab841034cbdcba626269e467
light-y4gami <Chechenia475@hotmail.com>
2023-04-18 11:37:05 -0400

    second commit

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -6,10 +6,19 @@ import { PokemonService } from '../services/pokemon.service';
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.css']
 })
+
 export class PokemonComponent implements OnInit {
 
+  placeholder: string = 'Find your favorite pekemon'
+
   name: string = '';
-  urlImage: string = ''
+  namePokemon: string = '';
+  typesPokemon: string = '';
+  experience: string = '';
+  weight: string = '';
+  base_stat: string = '';
+  effort: string = ''
+  urlImage: string = 'https://cdn.dribbble.com/users/956603/screenshots/6348374/pokeball.png';
 
   constructor(private pokemonService : PokemonService) {}
 
@@ -18,8 +27,14 @@ export class PokemonComponent implements OnInit {
   }
 
   search() {
-    this.pokemonService.getPokemon(this.name).subscribe((data: any) => {
-      this.urlImage = data.sprites.other.dream_world.front_default
+    this.pokemonService.getPokemon(this.name.toLowerCase()).subscribe((data: any) => {
+      this.urlImage = data.sprites.other.dream_world.front_default;
+      this.namePokemon = `Name: ${data.species.name}`;
+      this.typesPokemon = `Pokemon-Type: ${data.types[0].type.name}`;
+      this.experience = `Experience: ${data.base_experience}`;
+      this.weight = `Weight: ${data.weight}`;
+      this.base_stat = `Base-stat: ${data.stats[0].base_stat}`;
+      this.effort = `Effort: ${data.stats[0].effort}`;
     })
   }
 }
